b3a4b9023e32fc924f6fece42df724b68d3c3aa5
Aleksi <>
2021-11-24 20:17:29 +0200

    foundations for FCNs with clearer structure

--- neural_upscaler/upscaler.py
+++ neural_upscaler/upscaler.py
@@ -2,9 +2,6 @@ from neural_upscaler import image_processing
 
 def run(img_path, output_path, show_comparison=False):
     img = image_processing.read_image(img_path)
-    
-    img = crop_to_correct_dim(img)
-    img = fill_to_target_resolution(img)
     img = upscale(img)
     
     if show_comparison:
